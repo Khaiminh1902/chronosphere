72a63fb9a5d10bf0e6f51253fa3b7921690ede81
Khaiminh1902 <khaiminhnguyen1902@gmail.com>
2024-10-02 19:01:37 +0700

    October 2nd, 2024

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,17 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+import Logo from '@/public/logo.png'
+import { AuthModal } from './AuthModal'
+
+export default function Navbar() {
+  return (
+    <div className='flex py-5 items-center justify-between'>
+        <Link href="/" className='flex items-center gap-2'>
+            <Image src={Logo} alt="Logo" className='size-10' />
+            <h4 className='text-3xl font-semibold'>Chrono<span className='text-blue-500'>Sphere</span></h4>
+        </Link>
+        <AuthModal />
+    </div>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Navbar from './components/Navbar'
 
 const page = () => {
   return (
-    <div>page</div>
+    <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
+      <Navbar />
+    </div>
   )
 }
 
