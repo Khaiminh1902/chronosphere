c6a9602991fb6f701076c1fe1c61518524a4b936
Khaiminh1902 <khaiminhnguyen1902@gmail.com>
2024-10-07 17:15:16 +0700

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import Head from "next/head";
 import { ThemeProvider } from "./components/ThemeProvider";
 
 const geistSans = localFont({
